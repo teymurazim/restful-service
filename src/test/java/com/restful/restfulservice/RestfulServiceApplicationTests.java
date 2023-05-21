1cb12893fbe2dd1aa84e3281bd41565dec200fa7
teymurazim <teymur_azimzada@yahoo.com>
2023-05-21 01:12:08 -0400

    First commit

---
+++ src/test/java/com/restful/restfulservice/RestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful.restfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
